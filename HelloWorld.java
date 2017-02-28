e5aac399aea9d8c9a6b1272ea5507210377512d6
mahesh <mart2mahe@gmail.com>
2017-02-28 23:41:38 +0530

    "add HelloWorld"

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,9 @@
 public class HelloWorld {
 	public static void main(String[] args)
 	{
-		for(int i=0;i<10;i++)
+		for(int i=0;i<=10;i++)
 		{
-		System.out.println("Hello World!"+ i);
+		System.out.println("Hello World! This is Mahesh  |  "+ "Value of i: " + i);
 		}
     }
 }
